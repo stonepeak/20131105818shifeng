17ec8fc9d150b05cf9b1e92eb8d5b5fa5a6f32e6
stonepeak <dou@mac18.local>
2016-03-10 10:16:23 +0800

    Initial Commit

---
+++ 20131105818shifeng/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  20131105818shifeng
+//
+//  Created by dou on 16/3/10.
+//  Copyright © 2016年 stonepeak. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
