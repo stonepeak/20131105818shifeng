f3381afe1f11a9e103a89e32d5316760e8609642
stonepeak <dou@Mac18.local>
2016-03-10 11:49:07 +0800

    swift 语法for while if switch
    16749167491873941293

--- 20131105818shifeng/main.swift
+++ 20131105818shifeng/main.swift
@@ -8,5 +8,54 @@
 
 import Foundation
 
-print("Hello, World!")
+for var i in 1...5{
+    print("\(i)")
+}
+
+var n = 2
+while n < 100 {
+  n = n * 2
+}
+print("\(n)")
+
+var m = 2
+repeat {
+  m = m * 2
+} while m < 100
+print("\(m)")
+
+
+
+var t = 72
+if t <= 32 {
+    print("It's very cold. Consider wearing a scarf.")
+} else if t >= 86 {
+    print("It's really warm. Don't forget to wear sunscreen.")
+}else{
+    print("不冷不热")
+}
+
+var sw = 4
+switch sw {
+case 0:
+    print("0")
+case 1:
+    print("1")
+default:
+    print("3")
+}
+
+var e = 0
+switch e {
+case 0:
+    print("0")
+case 1:
+    "1"
+default:
+    "2"
+}
+
+
+
+
 
